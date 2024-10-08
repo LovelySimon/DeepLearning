be259e1fc4b0bdff9455e8b814c2c4e44ce8d877
wh603 <anlinjie87@gmail.com>
2024-10-08 15:12:26 +0800

    Mnist手写字识别完成

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # DeepLearning
 ### 用来保存我的深度学习路程
 
-完成了kaggle中泰坦尼克
+10.5  完成了kaggle中泰坦尼克titan.py
+
+10.8  在做mnist数据集手写识别mnist.py  **下午15：11 完成**
--- mnist.py
+++ mnist.py
@@ -2,9 +2,76 @@ import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision import datasets
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+import torch.optim as optim
 
-train_dataset = datasets.MNIST(root='../dataset/mnist', train=True, transform=transforms.ToTensor(), download=True)
-test_dataset = datasets.MNIST(root='../dataset/mnist', train=False, transform=transforms.ToTensor(), download=True)
+# 数据处理
+batch_size = 64
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (
+    0.3081,))])  # 将图像数据的矩阵进行转换成单通道Pytorch Tensor,并且对数据进行数据标准化，括号分别为均值和标准差
 
-train_loader = DataLoader(dataset=train_dataset, batch_size=32, shuffle=True)
-test_loader = DataLoader(dataset=test_dataset, batch_size=32, shuffle=False)
+train_dataset = datasets.MNIST(root='../dataset/mnist', train=True, transform=transform, download=True)
+test_dataset = datasets.MNIST(root='../dataset/mnist', train=False, transform=transform, download=True)
+
+train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+
+class MnistNet(torch.nn.Module):
+    def __init__(self):
+        super(MnistNet, self).__init__()
+        self.l1 = torch.nn.Linear(784, 512)
+        self.l2 = torch.nn.Linear(512, 256)
+        self.l3 = torch.nn.Linear(256, 128)
+        self.l4 = torch.nn.Linear(128, 64)
+        self.l5 = torch.nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 784)
+        x = F.relu(self.l1(x))
+        x = F.relu(self.l2(x))
+        x = F.relu(self.l3(x))
+        x = F.relu(self.l4(x))
+        return self.l5(x)
+
+
+model = MnistNet()
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+
+
+def train(epoch):
+    running_loss = 0.0
+    for batch_idx, data in enumerate(train_loader, 0):
+        inputs, target = data
+        optimizer.zero_grad()
+
+        outputs = model(inputs)
+        loss = criterion(outputs, target)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()  # 防止出现计算图
+        if batch_idx % 300 == 299:
+            print('[%d,%5d loss: %.3f' % (epoch + 1, batch_idx + 1, running_loss / 300))
+            running_loss = 0.0
+
+
+def test():
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, dim=1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print('Accuracy : %d %%' % (100 * correct / total))
+
+
+if __name__ == '__main__':
+    for epoch in range(10):
+        train(epoch)
+        test()
